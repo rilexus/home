4054b23d0887496409afb120efe39e259c590f47
Stanislav Panchenko <stanip@hotmail.de>
2019-01-12 14:39:46 +0100

    refactored popin component

--- client/src/pages/bio-page-component/bio-page.component.jsx
+++ client/src/pages/bio-page-component/bio-page.component.jsx
@@ -47,65 +47,70 @@ export class BioPageComponent extends React.Component{
 	closePopin(e){
         this.setPopinState(false);
     }
-
+    
+    getPopinContent(){
+		return (
+			<div className="password-wrapper">
+				<div className={'center'}>
+					<h2>Password required!</h2>
+					<p>please enter the provided password</p>
+				</div>
+				<div className="password-field">
+					<input className={'number-input'} type="number"/>
+					<input className={'number-input'} type="number"/>
+					<input className={'number-input'} type="number"/>
+					<input className={'number-input'} type="number"/>
+				</div>
+				<div className="send-button-wrapper">
+					<button onClick={(e)=>this.closePopin(e)}>
+						Download
+					</button>
+				</div>
+			</div>
+		);
+    }
 
 	render() {
 		return (
-			<div className={'bio-page-component'}
-				 // onClick={(e)=> this.closePopin(e)}
-			>
-				<div
-					className={['blur-wrapper', this.state.openPopin ? 'blur-wrapper blured' : null].join(' ')}
-				>
-                    <HeaderComponent/>
-                    <PageTitleComponent title={this.props.title} comment={''}/>
-                    <PageSubtitleComponent
-                        comment={this.props.comment}
-                        url={this.props.imgUrl}
-                        subtitle={this.props.subtitle}/>
-
-                    <div className="wrapper">
-	                    <SlideupAnimationComponent timeout={200} _in={true} duration={700}>
-		                    <FadeinAnimationComponent duration={1200} _in={true} timeout={400}>
-			                    <div className="text-container center">
-				                    <div className="text-title">
-					                    <h2>
-						                    Full CV
-					                    </h2>
-					                    <h3>
-						                    Download
-					                    </h3>
-				                    </div>
-				                    <p>
-					                    You can download my full CV here
-				                    </p>
-				                    <div className="download-button-wrapper">
-					                    <button
-						                    onClick={()=>this.openPopin()}
-						                    className={'download-button'}>
-						                    <LockIconComponent/>
-						                    Download
-					                    </button>
-				                    </div>
-			                    </div>
-		                    </FadeinAnimationComponent>
-	                    </SlideupAnimationComponent>
-                    </div>
-				</div>
-				<PopinComponent in_={this.state.openPopin} onClick={(e)=>this.closePopin(e)}>
-					<div className="password-wrapper">
-						<div className={'center'}>
-							<h2>Password required!</h2>
-							<p>please enter the provided password</p>
-						</div>
-						<div className="password-field">
-							<input className={'number-input'} type="number"/>
-							<input className={'number-input'} type="number"/>
-							<input className={'number-input'} type="number"/>
-							<input className={'number-input'} type="number"/>
-						</div>
-						<div className="send-button-wrapper">
-							<button>Download</button>
+			<div className={'bio-page-component'}>
+				<PopinComponent
+					in_={this.state.openPopin}
+					onClick={(e)=>this.closePopin(e)}
+					popinContent={()=>this.getPopinContent()}>
+					<div>
+						<HeaderComponent/>
+						<PageTitleComponent title={this.props.title} comment={''}/>
+						<PageSubtitleComponent
+							comment={this.props.comment}
+							url={this.props.imgUrl}
+							subtitle={this.props.subtitle}/>
+						
+						<div className="wrapper">
+							<SlideupAnimationComponent timeout={200} _in={true} duration={700}>
+								<FadeinAnimationComponent duration={1200} _in={true} timeout={400}>
+									<div className="text-container center">
+										<div className="text-title">
+											<h2>
+												Full CV
+											</h2>
+											<h3>
+												Download
+											</h3>
+										</div>
+										<p>
+											You can download my full CV here
+										</p>
+										<div className="download-button-wrapper">
+											<button
+												onClick={()=>this.openPopin()}
+												className={'download-button'}>
+												<LockIconComponent/>
+												Download
+											</button>
+										</div>
+									</div>
+								</FadeinAnimationComponent>
+							</SlideupAnimationComponent>
 						</div>
 					</div>
 				</PopinComponent>
--- client/src/shared/components/popin-component/popin.component.jsx
+++ client/src/shared/components/popin-component/popin.component.jsx
@@ -3,58 +3,61 @@ import {CSSTransition} from "react-transition-group";
 import './popin-component.style.scss';
 
 
-
-class PopinComponent extends React.Component{
-    state = {
-
-    };
-
-    componentDidMount(){
-
-    }
-
-    render(){
-        return(
-            <CSSTransition
-                in={this.props.in_}
-                appear
-                mountOnEnter
-                unmountOnExit
-                timeout={1}
-                classNames={'popin-component-appear-animation'}
-            >
-                <div className={'popin-component'}>
-                    <CSSTransition
-                        in={this.props.in_}
-                        appear
-                        mountOnEnter
-                        unmountOnExit
-                        timeout={10}
-                        classNames={'popin-bg-appear-animation'}>
-                        <div
-                            onClick={e => this.props.onClick(e)}
-                            className="popin-component-bg"
-                        />
-                    </CSSTransition>
-
-                    <CSSTransition
-                        in={this.props.in_}
-                        appear
-                        mountOnEnter
-                        unmountOnExit
-                        timeout={200}
-                        classNames={'popin-appear-animation'}
-                    >
-                        <div className="popin">
-                            {this.props.children}
-                        </div>
-                    </CSSTransition>
-                </div>
-            </CSSTransition>
-        )
-    }
+class PopinComponent extends React.Component {
+	state = {};
+	
+	componentDidMount() {
+	
+	}
+	
+	render() {
+		const visible = this.props.in_;
+		return (
+			<div className={'popin-component'}>
+				{/*render passed children instantly*/}
+				<div className={['page-content blur-wrapper', visible ? 'blured' : null].join(' ')}>
+					{this.props.children}
+				</div>
+				{/*render popin only on signal*/}
+				<CSSTransition
+					in={visible}
+					appear
+					mountOnEnter
+					unmountOnExit
+					timeout={10}
+					classNames={''}>
+					<div className={'popin-wrapper'}>
+						<CSSTransition
+							in={visible}
+							appear
+							mountOnEnter
+							unmountOnExit
+							timeout={10}
+							classNames={'popin-bg-appear-animation'}>
+							<div
+								onClick={e => this.props.onClick(e)}
+								className="popin-component-bg"
+							/>
+						</CSSTransition>
+						
+						<CSSTransition
+							in={visible}
+							appear
+							mountOnEnter
+							unmountOnExit
+							timeout={200}
+							classNames={'popin-appear-animation'}
+						>
+							<div className="popin">
+								{this.props.popinContent()}
+							</div>
+						</CSSTransition>
+					</div>
+				</CSSTransition>
+			</div>
+		)
+	}
 }
-
 
 
 export default PopinComponent;
